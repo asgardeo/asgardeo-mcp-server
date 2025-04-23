aefd7b6e6a07d1efe046ca821376525ff82179ea
Pavindu Lakshan <pavindulakshan@gmail.com>
2025-04-23 20:27:05 +0530

    add app mgt tools

--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 
-	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
-	"github.com/shashimalcse/go-asgardeo/management"
-	"github.com/thilinashashimalsenarath/asgardeo-mcp/internal/asgardeo"
-	"github.com/thilinashashimalsenarath/asgardeo-mcp/internal/config"
+	applications "github.com/thilinashashimalsenarath/asgardeo-mcp/internal/tools"
 )
 
 // setupServer configures the MCP server and registers tools.
-func setupServer(client *management.Client) *server.MCPServer {
+func setupServer() *server.MCPServer {
 	s := server.NewMCPServer(
 		"Asgardeo Management MCP",
 		"1.0.0",
@@ -22,37 +17,21 @@ func setupServer(client *management.Client) *server.MCPServer {
 		server.WithRecovery(),
 	)
 
-	// Register ListAsgardeoApplication tool
-	appList := mcp.NewTool("ListAsgardeoApplication",
-		mcp.WithDescription("List Asgardeo Applications"),
-	)
-	s.AddTool(appList, func(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-		resp, err := client.Applications().List(ctx, nil)
-		if err != nil {
-			log.Printf("Error listing applications: %v", err)
-			return nil, err
-		}
-		return mcp.NewToolResultText(fmt.Sprintf("%+v", resp)), nil
-	})
+	appListTool, appListToolImpl := applications.List()
+	s.AddTool(appListTool, appListToolImpl)
+
+	appDetailFetchTool, appDetailFetchToolImpl := applications.Get()
+	s.AddTool(appDetailFetchTool, appDetailFetchToolImpl)
+
+	appCreateTool, appCreateToolImpl := applications.Create()
+	s.AddTool(appCreateTool, appCreateToolImpl)
+
 	return s
 }
 
 func main() {
-	// Load and validate configuration
-	baseURL, clientID, clientSecret, err := config.Load()
-	if err != nil {
-		log.Fatalf("Configuration error: %v", err)
-	}
-
-	ctx := context.Background()
-	// Initialize Asgardeo client
-	client, err := asgardeo.NewClient(ctx, baseURL, clientID, clientSecret)
-	if err != nil {
-		log.Fatalf("Failed to create Asgardeo client: %v", err)
-	}
-
 	// Setup and start MCP server
-	s := setupServer(client)
+	s := setupServer()
 	if err := server.ServeStdio(s); err != nil {
 		log.Fatalf("Server error: %v", err)
 	}
