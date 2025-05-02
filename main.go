ddf242219eaaf387da4b20d19a7d8bdb38a5c51e
Thilina Shashimal Senarath <43197743+shashimalcse@users.noreply.github.com>
2025-05-02 12:49:41 +0530

    fix with new sdk changes (#3)

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"log"
 
-	"github.com/mark3labs/mcp-go/server"
 	"github.com/asgardeo/mcp/internal/tools"
+	"github.com/mark3labs/mcp-go/server"
 )
 
 // setupServer configures the MCP server and registers tools.
 func setupServer() *server.MCPServer {
 	s := server.NewMCPServer(
 		"Asgardeo Management MCP",
-		"1.0.0",
+		"0.0.1",
 		server.WithResourceCapabilities(true, true),
 		server.WithLogging(),
 		server.WithRecovery(),
@@ -19,21 +19,6 @@ func setupServer() *server.MCPServer {
 
 	appListTool, appListToolImpl := tools.GetApplicationListTool()
 	s.AddTool(appListTool, appListToolImpl)
-
-	appDetailFetchTool, appDetailFetchToolImpl := tools.GetApplicationDetailTool()
-	s.AddTool(appDetailFetchTool, appDetailFetchToolImpl)
-
-	appCreateTool, appCreateToolImpl := tools.GetApplicationCreateTool()
-	s.AddTool(appCreateTool, appCreateToolImpl)
-
-	apiResourceCreateTool, apiResourceCreateToolImpl := tools.GetApiResourceCreateTool()
-	s.AddTool(apiResourceCreateTool, apiResourceCreateToolImpl)
-
-	apiResourceListTool, apiResourceListToolImpl := tools.GetApiResourceListTool()
-	s.AddTool(apiResourceListTool, apiResourceListToolImpl)
-
-	apiResourcePatchTool, apiResourcePatchToolImpl := tools.GetApiResourcePatchTool()
-	s.AddTool(apiResourcePatchTool, apiResourcePatchToolImpl)
 
 	return s
 }
